30d6c5adb5320e656299e42dd3df289a75e60fc1
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2025-11-26 14:24:16 -0300

    Representando outras tabelas do arquivo erdplus

--- teste.sql
+++ teste.sql
@@ -17,4 +17,43 @@ CREATE TABLE dbo.localizacao (
   
  	CONSTRAINT cidade_fk
  		fOREIGN KEY (id_cidade) REFERENCES dbo.cidade(ID),
+  
+ 	CONSTRAINT localizacao_un UNIQUE (id_cidade)
 );
+
+CREATE TABLE dbo.tempo (
+	ID INTEGER GENERATED ALWAYS AS IDENTITY,
+ 	tempo_min INTEGER NOT NULL,
+ 	tempo_max INTEGER NOT NULL,
+	precip_pluviom REAL not NULL DEFAULT 0.0,
+ 	dh_coleta TIMESTAMP not NULL,
+ 	id_cidade INTEGER NOT NULL,
+  
+ 	CONSTRAINT tempo_pk PRIMARY KEY(ID),
+  
+ 	CONSTRAINT tempo_fk
+ 		FOREIGN KEY (id_cidade) REFERENCES dbo.cidade(id),
+  
+ 	CONSTRAINT dhColeta_idCidade_un UNIQUE (dh_coleta, id_cidade)
+);
+
+CREATE TABLE dbo.central (
+	codigo CHAR(5) NOT NULL,
+  
+ 	CONSTRAINT central_pk PRIMARY key (codigo)
+  
+);
+
+CREATE TABLE dbo.tempoCentral (
+	id_tempo INTEGER not NULL,
+ 	codigo_central CHAR(5) not NULL,
+  
+ 	CONSTRAINT tempocentral_pk PRIMARY KEY (id_tempo, codigo_central),
+  
+ 	CONSTRAINT tempo_fk 
+ 		FOREIGN KEY (id_tempo) REFERENCES dbo.tempo(id),
+  
+ 	CONSTRAINT central_fk
+ 		FOREIGN key (codigo_central) REFERENCES dbo.central(codigo)
+);
+ 
